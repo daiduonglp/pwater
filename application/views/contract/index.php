01e47ae897e137aa1e2955136a342c2fce5107ca
chi_cong <chi_cong@chi-cong-PC>
2016-05-12 09:30:54 +0700

    commit branch 111 contract

--- application/views/contract/index.php
+++ application/views/contract/index.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 This is typically reached through offer and an acceptance which does not vary the offer's terms, which is known as the "mirror image rule".
 
-If a purported acceptance does vary the terms of an offer, it is not an acceptance but a counteroffer and, therefore, simultaneously a rejection of the original offer.
+A bilateral contract is an agreement in which each of the parties to the contract makes a promise[4] or set of promises to each other., it is not an acceptance but a counteroffer and, therefore, simultaneously a rejection of the original offer.
 
 The Uniform Commercial Code disposes of the mirror image rule in §2-207, although the UCC only governs transactions in goods in the USA
     </div>
