e32beab5476bdb3542fe8bfc3e4c6d474859d8d6
chi_cong <chi_cong@chi-cong-PC>
2016-05-12 09:19:16 +0700

    commit contract

---
+++ application/views/contract/index.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>contract</title>
+</head>
+<body>
+    <div id="container">
+ In order for a contract to be formed, the parties must reach mutual assent (also called a meeting of the minds).
+
+This is typically reached through offer and an acceptance which does not vary the offer's terms, which is known as the "mirror image rule".
+
+If a purported acceptance does vary the terms of an offer, it is not an acceptance but a counteroffer and, therefore, simultaneously a rejection of the original offer.
+
+The Uniform Commercial Code disposes of the mirror image rule in §2-207, although the UCC only governs transactions in goods in the USA
+    </div>
+</body>
+</html>
