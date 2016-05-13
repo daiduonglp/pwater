129a0cb5a7dd9bd211fc90a16c8bc284070edfaa
lampart_hai_son <hai_son@lampart-vn.com>
2016-05-13 15:51:52 +0700

    commit delivery page

---
+++ application/views/delivery/index.php
@@ -0,0 +1,19 @@
+<style>
+      .delivery{
+        border-style: solid;
+        margin: 10 10 10 10;
+        padding: 20 20 20 20;
+      }
+      .delivery_title{
+        margin: 10 10 10 500;
+      }
+</style>
+
+<div class="delivery_title"> This is Delivery Page <br /> </div>
+<div class="delivery">
+Most consumer goods are delivered from a point of production (factory or farm) through one or more points of storage (warehouses) to a point of sale (retail store), where the consumer buys the good and is responsible for its transportation to point of consumption. There are many variations on this model for specific types of goods and modes of sale. Products sold via catalogue or the Internet may be delivered directly from the manufacturer or warehouse to the consumer's home, or to an automated delivery booth. Small manufacturers may deliver their products directly to retail stores without warehousing.
+<br /><br />
+Some manufacturers maintain factory outlets which serve as both warehouse and retail store, selling products directly to consumers at wholesale prices (although many retail stores falsely advertise as factory outlets). Building, construction, landscaping and like materials are generally delivered to the consumer by a contractor as part of another service. Some highly perishable or hazardous goods, such as radioisotopes used in medical imaging, are delivered directly from manufacturer to consumer.
+<br /><br />
+Home delivery is often available for fast food and other convenience products, e.g. pizza delivery.[2] Sometimes home delivery of supermarket goods is possible.[3] A milk float is a small battery electric vehicle (BEV), specifically designed for the delivery of fresh milk. A new form of delivery is emerging on the horizon of the internet age: Delivery by the crowd e.g. crowd delivery. In this concept an individual not necessarily contracted by the vendor performs the delivery of goods to the destination. Sometimes, private courier companies will also deliver consumer goods on a regular basis for companies like E-commerce businesses.[4]
+</div>
